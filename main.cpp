7f2967ba352b10af8edc69c09599e8dbb3f621c2
kingslygbulatao-web <kingslygbulatao@gmail.com>
2026-02-08 20:03:56 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+
+struct Restaurant
+{
+    int quantity;
+    std::string Address;
+    double discount; 
+    bool price;
+    int Rating; 
+};
+
+int main() {
+   
+    
+    Restaurant Mcdonalds = { 100, "123, Mcdonalds Str", .05, true, 22};
+
+
+    return 0;
+}
+
